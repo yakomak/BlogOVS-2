b6f8f449374665acaa1e8358f3cd908e11e55d44
Maxim <yakomak137@gmail.com>
2017-09-06 09:59:02 +0300

    Update server.js
    малейшее изменение )

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var exs = require("express");
 var app = exs();
 app.use(exs.static(__dirname));
 var port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(process.env.PORT || 3000);
 console.log(`start server on port ${port}`);
